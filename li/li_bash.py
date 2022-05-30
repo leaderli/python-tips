c7de8356cac40141a53b2ad18779f3618d96bb40
leaderli <429243408@qq.com>
2022-05-30 17:20:58 +0800

    sip

--- li/li_bash.py
+++ li/li_bash.py
@@ -5,5 +5,5 @@ def bash(command: str):
     with Popen(command, shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True) as fd:
         out, err = fd.communicate()
         if err:
-            raise RuntimeError(err.strip())
+            raise err.strip()
         return out.strip()
