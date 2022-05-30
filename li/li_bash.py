7f962aaf50bbc3af8d2aff5a8faab13b4164ff5b
leaderli <429243408@qq.com>
2022-05-30 17:17:43 +0800

    sip

--- li/li_bash.py
+++ li/li_bash.py
@@ -3,6 +3,7 @@ from subprocess import Popen, PIPE
 
 def bash(command: str):
     with Popen(command, shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True) as fd:
-        if fd.returncode != 0:
-            raise RuntimeError(fd.stderr.read())
-        return fd.stdout.read()
+        out, err = fd.communicate()
+
+        print(err)
+        return out.strip()
