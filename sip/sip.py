c68085a271abba464ffca09bb95ee8a528cd2f23
leaderli <429243408@qq.com>
2022-06-02 10:02:27 +0800

    20220602  100227

--- sip/sip.py
+++ sip/sip.py
@@ -101,7 +101,7 @@ class Sip(LiCmd):
 
     def complete_env(self, text: str, state: int):
 
-        return os.environ.keys()
+        return [k for k in os.environ.keys()]
 
 
 if __name__ == '__main__':
