5a5a8105e2ace4ec9d396af49be2146d90bc32fa
leaderli <429243408@qq.com>
2022-06-06 13:39:37 +0800

    20220606  133937

--- test/subprocess_test.py
+++ test/subprocess_test.py
@@ -1,11 +1,14 @@
-import subprocess
+from  li.li_bash import  ssh_call
+# import subprocess
+#
+# r = subprocess.Popen(['echo', 'hello'], stdout=subprocess.PIPE, universal_newlines=True)
+#
+# print('-' * 100)
+# print(r.stdout.read())
+# print('-' * 100)
+#
+# r = subprocess.Popen(['echo', 'hello'], stdout=subprocess.PIPE)
+# print(r.stdout.read())
+# print('-' * 100)
 
-r = subprocess.Popen(['echo', 'hello'], stdout=subprocess.PIPE, universal_newlines=True)
-
-print('-' * 100)
-print(r.stdout.read())
-print('-' * 100)
-
-r = subprocess.Popen(['echo', 'hello'], stdout=subprocess.PIPE)
-print(r.stdout.read())
-print('-' * 100)
+ssh_call('li@192.168.142.128','~','ll')
